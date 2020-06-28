17b9b6159e877c7a2b05b89731bf6364e4ff0a8b
Rafael Faria <rafaelsanfaria@gmail.com>
2020-06-28 17:27:08 -0300

    feat: controller SignUp returns 500 if emailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
-import { MissingParamError, InvalidParamError } from '../errors'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
@@ -11,16 +11,22 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,7 +1,9 @@
 import { InvalidParamError } from './invalid-param-error'
 import { MissingParamError } from './missing-param-error'
+import { ServerError } from './server-error'
 
 export {
   InvalidParamError,
-  MissingParamError
+  MissingParamError,
+  ServerError
 }
